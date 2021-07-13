2b34f43322c32c286ec408ff50930a1dbbc7341c
ajskateboarder <81255936+themysticsavages@users.noreply.github.com>
2021-07-13 09:43:36 -0700

    Bad code :P

---
+++ what.py
@@ -0,0 +1,8 @@
+pass; import sys; import os; from win32con import TRUE; print;(1);Warning;();'a\a';rnd=exec.__eq__(repr);''.__format__('');pass;xy=['1', 2, 't{}'.format('he. what')], property.__format__;sys.__dict__;x=1;print('...', end='{}'.format(''.format('\n...')));'//._.\\';y="""
+what is actually going on!""";print(x);del sys; sys='what i just deleted sys'; print(sys); 'lmao nothing gets printed anymore'; sys.replace('\n\n\n', f'{bool.__class__}'.format(True)); print(str(xy).replace('(', '').replace(')', '')
+.replace(", <method '__format__' of 'object' objects>", '').replace("'", '').replace('[', '').replace(']', '').split(' ')[2], str(xy).replace('(', '').replace(')', '').replace(", <method '__format__' of 'object' objects>", '').replace("'", '')
+.replace('[', '').replace(']', '').split(' ')[3]); os.chdir(os.getcwd()); del x; x = 1;pass;pass;pass;pass;pass; import sys; import os; import sys; import os, time; print('look at this code bruh'); time.sleep(1); print('it'); time.sleep(0.6); print('is'); 
+time.sleep(0.6); print('so'); time.sleep(0.6); print('bad'); time.sleep(0.6);del x; r=1; """what what what what what """+str(r+r)+""""""; print('I mean... '); time.sleep(0.7); print('it just barely works.'); time.sleep(0.7); print('im gonna crash now...system error.. :)');
+import win10toast; new = win10toast.ToastNotifier(); new.show_toast('nice knowing you', 'e', threaded=True); 
+with open('what.py', 'r') as g: x = g.read(); g.close(); 
+with open('code.txt', 'w') as g: g.write('This is my code\n'+x); bruh
